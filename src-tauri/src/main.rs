44ac9c20d80191e231f4b27c08564bdf474c8890
swaye chateau <swaye@kitechsoftware.com>
2023-12-08 09:46:48 +0000

    added system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,76 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn login(name: &str) -> String {
+    format!("Hello, {}! You've are now logged in!", name)
+}
+
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent};
+use tauri::Manager;
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let show = CustomMenuItem::new("show".to_string(), "Show");
+    let tray_menu = SystemTrayMenu::new()
+      .add_item(show)
+      .add_item(hide)
+      .add_native_item(SystemTrayMenuItem::Separator)
+      .add_item(quit);
+
+    let tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick {
+              position: _,
+              size: _,
+              ..
+            } => {
+              println!("system tray received a left click");
+            }
+            SystemTrayEvent::RightClick {
+              position: _,
+              size: _,
+              ..
+            } => {
+              println!("system tray received a right click");
+            }
+            SystemTrayEvent::DoubleClick {
+              position: _,
+              size: _,
+              ..
+            } => {
+              println!("system tray received a double click");
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => {
+              match id.as_str() {
+                "quit" => {
+                  std::process::exit(0);
+                }
+                "hide" => {
+                  let window = app.get_window("main").unwrap();
+                  window.hide().unwrap();
+                }
+                "show" => {
+                  let window = app.get_window("main").unwrap();
+                  window.show().unwrap();
+                }
+                _ => {}
+              }
+            }
+            _ => {}
+          })
+        .invoke_handler(tauri::generate_handler![greet, login])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
